a23a34377162b4b60ba78224eb3234a7c4df6257
Jia Wei <leejiawei241@gmail.com>
2025-01-18 16:05:30 +0800

    Added backend

--- browser-extension/game.js
+++ browser-extension/game.js
@@ -1,4 +1,5 @@
 import { domains } from "./domains.js";
+const params = new URLSearchParams(window.location.search);
 
 // Get question and answer from backend
 async function getQnAndAns() {
@@ -7,15 +8,10 @@ async function getQnAndAns() {
     lastFocusedWindow: true,
   });
   const { origin } = new URL(url);
-  const res = {
-    question: "What colour is the sky?",
-    correct: "blue",
-    wrong1: "purple",
-    wrong2: "pink",
-    wrong3: "yellow  ",
-  };
-
-  return res;
+  const res = await fetch(
+    `http://localhost:3000/get-question/${domains[origin]}/${params.get("difficulty")}`,
+  );
+  return await res.json();
 }
 
 let attemptsLeft = 2;
