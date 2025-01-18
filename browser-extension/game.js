163688470403398ef3c8a74205d2d5615019ef35
Jia Wei <leejiawei241@gmail.com>
2025-01-18 15:39:01 +0800

    Added function to get qn and ans from backend, added dummy data

--- browser-extension/game.js
+++ browser-extension/game.js
@@ -1,1 +1,24 @@
 import { domains } from "./domains.js";
+
+// Get question and answer from backend
+async function getQnAndAns() {
+  const [{ url }] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  const { origin } = new URL(url);
+  const res = {
+    question: "What colour is the sky?",
+    correct: "blue",
+    wrong1: "purple",
+    wrong2: "pink",
+    wrong3: "yellow  ",
+  };
+
+  return res;
+}
+
+let attemptsLeft = 2;
+const numOfTriesElement = document.getElementById("numberOfTries");
+document.getElementById("numberOfTries").innerHTML =
+  `Numbers of attempts left: ${attemptsLeft}`;
